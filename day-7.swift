eaf21efa28cad2a098b2f27b82b8052e91d946be
Ahmet Enes Irmak <AhmetEnesIrmak@gmail.com>
2022-08-31 09:39:37 +0300

    Day 7: Arrays

---
+++ day-7.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+//My Solution
+var newArr = Array(arr.reversed())
+newArr.forEach{print($0, terminator: " ")}
+
+
