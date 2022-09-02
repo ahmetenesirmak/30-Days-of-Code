5305f724847fbadc01da6387b14a9820e101bada
Ahmet Enes Irmak <AhmetEnesIrmak@gmail.com>
2022-09-02 08:33:22 +0300

    Day 9: Recursion 3

---
+++ day-9.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+//My Solution
+var total = 1
+
+func factorial(n: Int) -> Int {
+    for i in 1...n {
+        total *= i
+    }
+    return total
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+let result = factorial(n: n)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
